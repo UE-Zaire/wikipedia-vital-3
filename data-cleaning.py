a4415cf2be00ea7f8c64372571909e1144f8869f
Ian Pradhan <Ian>
2018-05-04 09:23:51 -0400

    Remove unwanted newlines

--- data-cleaning.py
+++ data-cleaning.py
@@ -8,13 +8,13 @@ for filename in os.listdir(os.getcwd() + '\\pages'):
     page_links = open('pages/' + filename, 'r')
 
     for line in page_links:
-        links.append([filename[:-4].replace(' ', '_'), line])
+        links.append([filename[:-4].replace(' ', '_'), line[:-1]])
 
 print(len(links))
 
 links_file = open('links.txt', 'w')
 for link in links:
-    links_file.write(','.join(link))
+    links_file.write(','.join(link) + '\n')
 links_file.close()
 
 links_json = open('links.json', 'w')
